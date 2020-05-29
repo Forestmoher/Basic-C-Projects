105aae10f7dd31677e312ff0ecad61e095e02243
Forest Moher <fomoher@gmail.com>
2020-05-29 12:19:31 -0700

    Add Program Files
    Added examples of class methods with simple calculator class and math operations

---
+++ MethodsInC_Sharp/MethodsInC_Sharp/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsInC_Sharp
+{
+    class Calculator
+    {
+        public int value { get; set; }
+
+        public int Multiply(int numOne, int numTwo)
+        {
+            int answerOne = numOne * numTwo;
+            return answerOne;
+        }
+
+        public int Divide(int numOne, int numTwo)
+        {
+            int answerTwo = numOne / numTwo;
+            return answerTwo;
+        }
+
+        public int Add(int numOne, int numTwo)
+        {
+            int answerThree = numOne + numTwo;
+            return answerThree;
+        }
+    }
+}
---
+++ MethodsInC_Sharp/MethodsInC_Sharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsInC_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            try
+            {
+                Console.WriteLine("We are gonna do some simple math operations");
+                Console.WriteLine("Enter a number:");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a another number:");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Calculating.........");
+                Console.ReadLine();
+
+                int answerOne = calculator.Multiply(numOne, numTwo);
+                int answerTwo = calculator.Divide(numOne, numTwo);
+                int answerThree = calculator.Add(numOne, numTwo);
+
+                Console.WriteLine(numOne + " times " + numTwo + " equals " + answerOne);
+                Console.WriteLine(numOne + " divided by " + numTwo + " equals " + answerTwo);
+                Console.WriteLine(numOne + " plus " + numTwo + " equals " + answerThree);
+                Console.ReadLine();
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a valid whole number");
+            }
+            finally
+            {
+                Console.ReadLine();
+
+            }
+
+
+        }
+    }
+}
