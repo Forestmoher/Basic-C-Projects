fee0983a3471abc1d8ea398a05268e530c87f843
Forest Moher <fomoher@gmail.com>
2020-05-31 08:35:14 -0700

    Add Program Files
    Add basic examples of Method Overloading

---
+++ MethodOverloading/MethodOverloading/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Calculator
+    {
+        public int tipCalculator(int num)
+        {
+            int answer = (num / 5) + num;
+            return answer;
+        }
+
+        public int tipCalculator(decimal tip, decimal cost)
+        {
+            int answer = Convert.ToInt32((tip * cost) + cost);
+            return answer;
+        }
+
+        public int tipCalculator(string cost, string people)
+        {
+            int answer = Convert.ToInt32(cost) / Convert.ToInt32(people);
+                return answer;
+        }
+    }
+
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            //method takes in an integer
+            Console.WriteLine("Enter the cost of the meal and we will add a 20% tip");
+            int cost = Convert.ToInt32(Console.ReadLine());
+            int total = calculator.tipCalculator(cost);
+            Console.WriteLine("The total cost of the meal is " + total);
+            Console.ReadLine();
+   
+            //method takes in two decimal values
+            Console.WriteLine("Enter in the cost of the meal:");
+            decimal costTwo = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter the tip percentage you want (.10,.18,.20)");
+            decimal tip = Convert.ToDecimal(Console.ReadLine());
+
+            total = Convert.ToInt32(calculator.tipCalculator(tip, costTwo));
+            Console.WriteLine("The total including tip is " + total);
+            Console.ReadLine();
+
+            //method takes in three strings 
+            Console.WriteLine("Enter in the cost of the meal:");
+            string costThree = Console.ReadLine();
+            Console.WriteLine("Enter in the number of people in your party:");
+            string people = Console.ReadLine();
+            total = Convert.ToInt32(calculator.tipCalculator(costThree, people));
+            Console.WriteLine("The cost per person is " + total);
+            Console.ReadLine();
+        }
+    }
+}
