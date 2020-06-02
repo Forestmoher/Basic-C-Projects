74c0ffb8aacc0386a3b370ed414c19a0734d87b0
Forest Moher <fomoher@gmail.com>
2020-06-02 14:08:55 -0700

    Add Program Files
    Add basic example of void method

---
+++ VoidMethod/VoidMethod/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Class1
+    {
+        public void voidMethod(int numOne, int numTwo)
+        {
+            int total = numOne * 10;
+            Console.WriteLine(numOne + " times ten equals: " + total);
+            Console.WriteLine(numTwo);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number:");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Class1 voidExample = new Class1();
+            voidExample.voidMethod(numOne: numOne, numTwo: numTwo);
+            Console.ReadLine();
+        }
+    }
+}
