f871243e9a850bda0cd43a45d895b1be39f3cc79
Forest Moher <fomoher@gmail.com>
2020-06-06 19:13:26 -0700

    Add Program Files

---
+++ AccountingWelcomePage/AccountingWelcomePage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace AccountingWelcomePage
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, We are \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
