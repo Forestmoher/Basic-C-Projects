d66b7badbf7f2122f6103cdf33eae27e1d7d6cf9
Forest Moher <fomoher@gmail.com>
2020-06-08 13:38:46 -0700

    Add Program Files
    Add example of operator overloading

---
+++ OperatorOverloading/OperatorOverloading/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+
+        public static string operator ==(Employee employeeOne, Employee employeeTwo)
+        {
+            string outcome;
+            if (employeeOne.ID == employeeTwo.ID)
+            {
+                outcome = "**Employees have same employee ID";               
+            }
+            else
+            {
+                outcome = "**Employees have unique employee IDs";
+            }
+            return outcome;
+        }
+        public static string operator !=(Employee employeeOne, Employee employeeTwo)
+        {
+            string outcome;
+            if (employeeOne.ID != employeeTwo.ID)
+            {
+                outcome = "**Employees have unique employee IDs";
+            }
+            else
+            {
+                outcome = "**Employees have same employee ID";
+            }
+            return outcome;
+        }
+
+    }
+}
---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Forest = new Employee()
+            {
+                FirstName = "Forest",
+                LastName = "Moher",
+                ID = 001
+            };
+            Employee Tom = new Employee()
+            {
+                FirstName = "Tom",
+                LastName = "Elam",
+                ID = 002
+            };
+            string outcome = Tom == Forest;
+            Console.WriteLine(outcome);
+            Console.ReadLine();
+
+        }
+    }
+}
