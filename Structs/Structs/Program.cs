ad59347470c4d1bfe3b5eef72aa723ef8e2fa540
Forest Moher <fomoher@gmail.com>
2020-06-09 12:42:19 -0700

    Add Program Files
    Example of struct

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number Number = new Number();
+            Number.Amount = 3.14m;
+            Console.WriteLine(Number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
